741630c507e3dbe568dcdaada8510b24d2fc1e0f
JaredUrquia2010a2010 <jaredurquia814@gmail.com>
2026-02-05 21:59:46 -0600

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function showTable(tableId) {
-    document.getElementById('clasificacion').style.display = 'block';
-    document.getElementById('goleadores').style.display = 'block';
-    document.getElementById(tableId).style.display = 'block';
-}
